42aff184c3aee84e5e821327d9a3a60cc6d548d0
Azat Nobatov <azatprog@gmail.com>
2018-03-11 17:37:08 +0300

    authService public

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from './services/auth.service';
 export class AppComponent {
   isLoggedIn = false;
 
-  constructor( private authService: AuthService ) {
+  constructor( public authService: AuthService ) {
     
   }
 }
