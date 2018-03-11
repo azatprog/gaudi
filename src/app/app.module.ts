92e30fb047e1102966230f2da4d67c1269021d3a
Azat Nobatov <azatprog@gmail.com>
2018-03-11 07:16:01 +0300

    view - login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ROUTES } from './app.routes';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
   ],
   imports: [
+    AngularFontAwesomeModule,
     BrowserModule,
     RouterModule.forRoot(ROUTES, { useHash: true })
   ],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 
 export const ROUTES: Routes = [
-    { path: '', component: AppComponent}
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'main', component: AppComponent,
+        children: [
+            { path: '', redirectTo: 'login', pathMatch: 'full'},
+            { path: 'login', component: LoginComponent }
+        ]
+    },
     // { path: 'login', component: LoginComponent },
-    // { path: '**', component: LoginComponent },
+    { path: '**', component: LoginComponent },
 ];
