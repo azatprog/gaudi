c1d366a04a315fd614870ec51d4c6dd897e6112e
Azat Nobatov <azatprog@gmail.com>
2018-03-11 08:44:20 +0300

    view - sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ROUTES } from './app.routes';
@@ -6,19 +7,27 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AuthService } from './services/auth.service';
+import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './main/sidebar/sidebar.component';
+import { ContentComponent } from './main/content/content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    MainComponent,
+    SidebarComponent,
+    ContentComponent,
   ],
   imports: [
     AngularFontAwesomeModule,
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(ROUTES, { useHash: true })
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,17 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
 
 export const ROUTES: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'main', component: AppComponent,
+    { path: 'app', component: AppComponent,
         children: [
             { path: '', redirectTo: 'login', pathMatch: 'full'},
-            { path: 'login', component: LoginComponent }
+            { path: 'login', component: LoginComponent },
+            { path: 'main', component: MainComponent }
         ]
     },
-    // { path: 'login', component: LoginComponent },
     { path: '**', component: LoginComponent },
 ];
