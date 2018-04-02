3398bde57d43c84e5e622b1266a4dc5f43607506
Azamat Sadr <sadr_17@mil.ru>
2018-04-02 14:15:39 +0300

    - Added random speed limits in points

--- front-end/src/app/services/map.service.ts
+++ front-end/src/app/services/map.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { AppSettings } from '../app.settings';
 
 export class Point {
   public lng: number;
   public lan: number;
   public elevation: number;
+  public speedLimit: number;
 
   constructor(lng: number, lan: number) {
     this.lng = lng;
@@ -44,28 +46,25 @@ export class MapService {
         if (status == google.maps.DirectionsStatus.OK) {       
           if (result.routes.length > 0) {
             var route = result.routes[0];
-            var routeDetails: RouteDetails  = new RouteDetails();
-            routeDetails.distance = route.legs[0].distance.value;
-            routeDetails.distanceDescription = route.legs[0].distance.text;
-            routeDetails.duration = route.legs[0].duration.value;
-            routeDetails.durationDescription = route.legs[0].duration.text;
-            routeDetails.start = route.legs[0].start_address;
-            routeDetails.end = route.legs[0].end_address;
-            routeDetails.route = route.overview_path.map(p => { return new Point(p.lng(), p.lat()) });
+            var routeDetails: RouteDetails  = MapService.convertPathToRoute(route);
 
             var elevator = new google.maps.ElevationService();
             let elevatorRequest: google.maps.PathElevationRequest = <google.maps.PathElevationRequest>{
               path: route.overview_path,
               samples: route.overview_path.length,
             }; 
+
             elevator.getElevationAlongPath(elevatorRequest, (result, status) => {
               if (status == google.maps.ElevationStatus.OK) {
-                routeDetails.route.map((p, i) => p.elevation = result[i].elevation);
+                routeDetails.route.map((p, i) => {
+                  p.elevation = result[i].elevation;
+                  p.speedLimit = Math.floor(Math.random() * (120 - 40 + 1)) + 40;
+                });
                 resolve(routeDetails);
               } else {
                 reject("Error: bad answer " + status);
               }
-            });         
+            });           
           } else {
             reject("Error: route didn't find");
           }
@@ -76,26 +75,15 @@ export class MapService {
     });
   }
 
-  getCoordinates(point): Promise<[number][number]> {
-    if (this.geocoder == null) {
-      this.geocoder = new google.maps.Geocoder();
-    }
-    
-    return new Promise<[number][number]>((resolve, reject) => {
-    this.geocoder.geocode( {'address': point},
-          (results: google.maps.GeocoderResult[], status: google.maps.GeocoderStatus) => {
-            if(status == google.maps.GeocoderStatus.OK) {
-              if (results.length > 0) {               
-                var xCenter = results[0].geometry.bounds.getCenter().lng();
-                var yCenter = results[0].geometry.bounds.getCenter().lat();
-                resolve([xCenter][yCenter]);
-              } else {            
-              reject("Error: point didn't find");
-              }
-            } else {
-              reject("Error: bad answer " + status);
-            }
-      });
-    });
+  static convertPathToRoute(route: google.maps.DirectionsRoute): RouteDetails {
+    var routeDetails: RouteDetails  = new RouteDetails();
+    routeDetails.distance = route.legs[0].distance.value;
+    routeDetails.distanceDescription = route.legs[0].distance.text;
+    routeDetails.duration = route.legs[0].duration.value;
+    routeDetails.durationDescription = route.legs[0].duration.text;
+    routeDetails.start = route.legs[0].start_address;
+    routeDetails.end = route.legs[0].end_address;
+    routeDetails.route = route.overview_path.map(p => { return new Point(p.lng(), p.lat()) });
+    return routeDetails;
   }
 }
