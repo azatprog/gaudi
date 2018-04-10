4d0856c96ac8a809248b4b1ed0235195ceedbfa5
gordinmitya <gordin.mitya@gmail.com>
2018-04-10 11:52:10 +0300

    in love with CI

--- back-end/src/main/scala/controllers/ViewController.scala
+++ back-end/src/main/scala/controllers/ViewController.scala
@@ -6,7 +6,7 @@ import org.scalatra.ScalatraServlet
 class ViewController extends ScalatraServlet {
   get("/") {
     contentType="text/html"
-    //DbSchema.initDb()
-    "Fleet Maintenance Prognostic System (db has been initialized)"
+    // DbSchema.initDb()
+    "Hello! I'm Fleet Maintenance Prognostic System and <h1>I love the GAUDI team!</h1>"
   }
 }
