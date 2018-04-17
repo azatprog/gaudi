405fd0d49a210874d6e42395173fcb7542363188
gordinmitya <gordin.mitya@gmail.com>
2018-04-17 21:28:10 +0300

    hardcoded user creds

--- front-end/src/app/login/login.component.ts
+++ front-end/src/app/login/login.component.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
   @ViewChild('f') form: any;
+  user: User;
 
   constructor(  private authService: AuthService,
-                private router: Router ) { }
+                private router: Router ) {
+                  this.user = new User();
+                 }
 
   ngOnInit() {
   }
 
   login() {
-    this.authService.isLoggedIn = true;
-    this.router.navigate(['/app/main']);
+    if (this.user.login === 'admin' && this.user.password === 'admin') {
+      this.authService.isLoggedIn = true;
+      this.router.navigate(['/app/main']);
+    } else {
+      alert('You, HACKER! Get lost.. :)');
+    }
+    
   }
 
 }
